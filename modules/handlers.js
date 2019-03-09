0df421cbb10828f354f2d628edabe386ea0d714e
Lukas <lukassh@gmail.com>
2019-03-09 12:09:28 +0100

    fix errors

--- modules/handlers.js
+++ modules/handlers.js
@@ -13,6 +13,7 @@ exports.upload = function(request, response) {
     });
 }
 
+
 exports.welcome = function(request, response) {
     console.log("Rozpoczynam obsługę żądania welcome.");
     fs.readFile('templates/start.html', function(err, html) {
@@ -26,4 +27,12 @@ exports.error = function(request, response) {
     console.log("Nie wiem co robić.");
     response.write("404 :(");
     response.end();
+}
+
+exports.show = function(request, response) {
+    fs.readFile("test.png", "binary", function(error, file) {
+        response.writeHead(200, {"Content-Type": "image/png"});
+        response.write(file, "binary");
+        response.end();
+    });
 }
