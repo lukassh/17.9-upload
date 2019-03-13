472b3e1524048082fc9df7b7f5aab5cb9c371e9c
Lukas <lukassh@gmail.com>
2019-03-13 21:31:05 +0100

    add style & upload.html

--- modules/handlers.js
+++ modules/handlers.js
@@ -3,16 +3,12 @@ var formidable = require('formidable');
 
 exports.upload = function(request, response) {
     console.log("Rozpoczynam obsługę żądania upload.");
-    var form = new formidable.IncomingForm();
-    form.parse(request, function(error, fields, files) {
-        fs.renameSync(files.upload.path, "test.png");
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write("received image:<br/>");
-        response.write("<img src='/show' />");
+    fs.readFile('templates/upload.html', function(err, html) {
+      response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+        response.write(html);
         response.end();
-    });
+    });     
 }
-
 
 exports.welcome = function(request, response) {
     console.log("Rozpoczynam obsługę żądania welcome.");
@@ -27,6 +23,17 @@ exports.error = function(request, response) {
     console.log("Nie wiem co robić.");
     response.write("404 :(");
     response.end();
+}
+exports.styles = function(request, response) {
+    console.log('Wysylam style');
+    var cssFile;
+    fs.readFile('templates/style.css', function(err, data) {
+        if (err){
+            throw err;
+        }
+        response.write(data);
+        response.end();
+    });
 }
 
 exports.show = function(request, response) {
--- modules/server.js
+++ modules/server.js
@@ -17,6 +17,9 @@ function start() {
         case '/upload':
             handlers.upload(request, response);
             break;
+        case "/styles" :
+            handlers.styles(request, response);
+            break;
         case '/show':
             handlers.show(request, response);
             break;    
